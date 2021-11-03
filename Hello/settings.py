76f9f835d9b764168e38e43ec6d60b941ec2c40f
UJWAL PANKAJ JAIN <ujwalsrm2000@gmail.com>
2021-11-03 22:15:17 +0530

    Initial

--- Hello/settings.py
+++ Hello/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a=76!*dmoeurh2@kg3-nnz5g-llm1*h8^($wh435lxyh2g3y60'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- Hello/urls.py
+++ Hello/urls.py
@@ -23,5 +23,7 @@ admin.site.index_title = "Welcome to UJ Foods Portal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('home.urls'))
+    path('',include('home.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
