730d2aaa0537bb979e1d3586987ce857c816cea5
UJWAL PANKAJ JAIN <ujwalsrm2000@gmail.com>
2021-11-03 22:31:45 +0530

    Update urls.py

--- Hello/urls.py
+++ Hello/urls.py
@@ -15,6 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.conf.urls import url
 
 admin.site.site_header = "UJ Foods Admin"
 admin.site.site_title = "UJ Foods Admin Portal"
